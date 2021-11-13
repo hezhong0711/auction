3eff2b5628548c950804af6d70c00e804a8f4ee5
HeZhong <zhong.he@thoughtworks.com>
2021-11-13 13:41:47 +0800

    [HeZhong] #N/A: init project

---
+++ src/main/java/com/tw/AuctionApplication.java
@@ -0,0 +1,13 @@
+package com.tw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class AuctionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionApplication.class, args);
+    }
+}
