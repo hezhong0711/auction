a971440bd46c9cd61d517d0122945cd96016829e
HeZhong <zhong.he@thoughtworks.com>
2021-11-13 16:38:01 +0800

    [HeZhong] #story-1 AC1 工序3: add auction item repository for pay margin.

---
+++ src/test/java/com/tw/auction/repository/AuctionApplyRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.tw.auction.repository;
+
+import com.tw.auction.base.TestBase;
+import com.tw.enums.MarginStatus;
+import com.tw.repository.AuctionApply;
+import com.tw.repository.AuctionApplyRespository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class AuctionApplyRepositoryTest extends TestBase {
+    @Autowired
+    AuctionApplyRespository auctionApplyRespository;
+
+    @Test
+    public void should_save_auction_apply_success_given_status_is_not_pay() {
+        AuctionApply auctionApply = AuctionApply.builder()
+                .accidentItemId(1L)
+                .marginPrice(BigDecimal.valueOf(2000))
+                .marginStatus(MarginStatus.NOT_PAY)
+                .build();
+
+        AuctionApply savedAuctionApply = auctionApplyRespository.save(auctionApply);
+        assertNotNull(savedAuctionApply);
+        assertEquals(MarginStatus.NOT_PAY, savedAuctionApply.getMarginStatus());
+    }
+}
